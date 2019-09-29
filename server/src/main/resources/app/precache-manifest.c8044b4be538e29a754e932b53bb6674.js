82f030f64cd50bbcaef313d8275fff2bcbbbc7f9
ccoke <1148120062@qq.com>
2019-09-29 11:20:28 +0800

    first

---
+++ server/src/main/resources/app/precache-manifest.c8044b4be538e29a754e932b53bb6674.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163fb0c40cbc3fbfb21e3dbc5f03a088",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4a8f4271cc7734fba34",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5dbc9eb86de86e15a7dd",
+    "url": "/static/js/2.21421202.chunk.js"
+  },
+  {
+    "revision": "e4a8f4271cc7734fba34",
+    "url": "/static/js/main.fc63edc9.chunk.js"
+  },
+  {
+    "revision": "607800751f5508d45615",
+    "url": "/static/js/runtime-main.04e95355.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
